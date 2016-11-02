5aee3fd805fdf1124b9541d338757fe123f0ecca
boramkim <brmie@hanmail.net>
2016-11-02 18:46:56 +0900

    after mvn package

--- src/test/java/com/board/practice/BoardTest.java
+++ src/test/java/com/board/practice/BoardTest.java
@@ -11,25 +11,25 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.board.practice.domain.BoardVO;
 import com.board.practice.persistance.BoardDAO;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml"  })
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"  })
 public class BoardTest {
-	
-	@Inject
-	private BoardDAO dao;
-	
-	@Test
-	public void insertTest() throws Exception{
-		BoardVO board = new BoardVO();
-		
-		board.setTitle("제목쿠");
-		board.setContent("hoho");
-		board.setWriter("보라미");
-		
-		Assert.assertEquals(1, dao.insertBoard(board));
-	}
-	
-	@Test
-	public void listTest() throws Exception{
-	}
+//	
+//	@Inject
+//	private BoardDAO dao;
+//	
+//	@Test
+//	public void insertTest() throws Exception{
+//		BoardVO board = new BoardVO();
+//		
+//		board.setTitle("제목쿠");
+//		board.setContent("hoho");
+//		board.setWriter("보라미");
+//		
+//		Assert.assertEquals(1, dao.insertBoard(board));
+//	}
+//	
+//	@Test
+//	public void listTest() throws Exception{
+//	}
 }
