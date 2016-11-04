0c64e2ed78b33f0dac60bcf7eb8390ffdb37a71b
boramkim <brmie@hanmail.net>
2016-11-05 05:07:05 +0900

    edit security

--- src/main/java/com/board/practice/controller/CommonController.java
+++ src/main/java/com/board/practice/controller/CommonController.java
@@ -1,5 +1,7 @@
 package com.board.practice.controller;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +10,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CommonController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String main(){
+		return "main";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String admin(Locale locale){
+		return "main";
+	}
+	
+	@RequestMapping(value="/main", method=RequestMethod.POST)
+	public String mainpost(){
 		return "main";
 	}
 }
